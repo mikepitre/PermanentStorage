78bcfae0b68d0f7adc5bbf87096b1c4d2cfd45c6
mikepitre <michaelepitre@gmail.com>
2015-09-22 12:47:50 -0500

    working with permanent storage

---
+++ PermanentStorage/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  PermanentStorage
+//
+//  Created by Mike Pitre on 9/22/15.
+//  Copyright © 2015 Mike Pitre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        NSUserDefaults.standardUserDefaults().setObject("Mike", forKey: "name")
+        
+        let userName = NSUserDefaults.standardUserDefaults().objectForKey("name")!
+        
+        print(userName)
+        
+        let arr = [1, 2, 3, 4]
+        
+        NSUserDefaults.standardUserDefaults().setObject(arr, forKey: "array")
+        
+        let returnedArray = NSUserDefaults.standardUserDefaults().objectForKey("array")! as! NSArray
+        
+        for i in returnedArray {
+            print(i)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
